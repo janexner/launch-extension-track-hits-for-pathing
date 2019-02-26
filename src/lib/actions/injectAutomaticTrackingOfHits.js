9c31ab89adf0696365b78374374096d9f0d6188d
Jan Exner <jexner@adobe.com>
2019-02-26 21:03:17 +0100

    in which our hero fixes validation

--- src/lib/actions/injectAutomaticTrackingOfHits.js
+++ src/lib/actions/injectAutomaticTrackingOfHits.js
@@ -1,43 +1,51 @@
 'use strict';
 
 var variableName = turbine.getExtensionSettings().prop;
-turbine.logger.info('Using ' + variableName);
+var contextDataVariable = turbine.getExtensionSettings().contextDataVariable;
+try {
+  turbine.logger.info('Using ' + variableName + "/" + contextDataVariable);
+} catch(e) {}
 
 var getTracker = turbine.getSharedModule("adobe-analytics","get-tracker");
 
 getTracker().then( function(s) {
   if (s) {
-    turbine.logger.info('Making new doPlugins...');
     var callOriginalDoPlugins = false;
     if ('function' === typeof s.doPlugins) {
       s.doPluginsOriginal = s.doPlugins;
       callOriginalDoPlugins = true;
-      turbine.logger.info('Stashed original doPlugins.');
-    } else {
-      turbine.logger.info('No original doPlugins found.');
     }
-    turbine.logger.info('Zwischenzeit');
     s.usePlugins = true;
     s_doPlugins = function(s) {
       // set prop
-      _satellite.logger.info('Kicking off new doPlugins...')
       if ('d' === s.linkType || 'e' === s.linkType || 'o' === s.linkType) {
+        var val;
         if (s.linkName) {
-          s[variableName] = s.linkName;
+          val = s.linkName;
         } else if ('d' === s.linkType) {
-          s[variableName] = "Download Link";
+          val = "Download Link";
         } else if ('e' === s.linkType) {
-          s[variableName] = "Exit Link";
+          val = "Exit Link";
         }
-        s.linkTrackVars = s.linkTrackVars ? s.linkTrackVars + ',' : '';
-        s.linkTrackVars = s.linkTrackVars + variableName;
+        if ('undefined' !== variableName && variableName) {
+          s[variableName] = val;
+          s.linkTrackVars = s.linkTrackVars ? s.linkTrackVars + ',' : '';
+          s.linkTrackVars = s.linkTrackVars + variableName;
+        }
+        if ('undefined' !== contextDataVariable && contextDataVariable) {
+          s['contextData'][contextDataVariable] = val;
+        }
       } else {
-        s[variableName] = 'D=pageName';
+        var val = "D=pageName";
+        if ('undefined' !== variableName && variableName) {
+          s[variableName] = val;
+        }
+        if ('undefined' !== contextDataVariable && contextDataVariable) {
+          s['contextData'][contextDataVariable] = val;
+        }
       }
-      _satellite.logger.info('Modified s object', s);
       if (callOriginalDoPlugins) {
         // call original doPlugins, too
-        _satellite.logger.info('Calling original doPlugins...');
         s.doPluginsOriginal.apply(this, s);
       }
     }
@@ -48,5 +56,5 @@ getTracker().then( function(s) {
 });
 
 module.exports = function(settings) {
-  turbine.logger.info('exports called.');
+  // nothing TODO here
 };
